548ae6a466a0bac92ec4c7a3a105ad9a4dc2dfa4
fateme <fateme3611@yahoo.com>
2021-08-30 18:50:52 -0400

    add bodyparser

--- express_server.js
+++ express_server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
