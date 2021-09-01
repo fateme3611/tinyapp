0ec12e34c2ef655595281f1f0870e1629f12959e
fateme <fateme3611@yahoo.com>
2021-09-01 18:30:26 -0400

    handel shortURL request

--- express_server.js
+++ express_server.js
@@ -8,6 +8,12 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+
+}
+
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -35,7 +41,11 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-app.post("/urls", (req, res) => {
+app.post("/urls/:shortURL", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
 });
